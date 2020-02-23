dbe7e35c5105b4754ac37e720ff04fd267faf8f9
Josh Unwin <joshunwin@gmail.com>
2020-02-23 16:57:43 +0000

    removed randomiser, doesn't work properly live

--- src/components/layout.js
+++ src/components/layout.js
@@ -22,8 +22,8 @@ const Container = styled.div`
 
 const Layout = ({ children }) => {
   const themes = ["light", "dark"];
-  const randomThemeSelection = themes[Math.floor(Math.random() * themes.length)];
-  const [selectedTheme, setSelectedTheme] = useState(randomThemeSelection)
+  // const randomThemeSelection = themes[Math.floor(Math.random() * themes.length)];
+  const [selectedTheme, setSelectedTheme] = useState('light')
   library.add(fab, faEnvelope)
 
   return (
