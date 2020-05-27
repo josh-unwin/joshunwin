d71a6c119b0a776135911f16f6275c9873716080
Josh Unwin <joshunwin@gmail.com>
2020-05-27 12:23:17 +0200

    removed theme color application from manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,6 +56,7 @@ module.exports = {
         start_url: `/`,
         background_color: `white`,
         theme_color: `white`,
+        theme_color_in_head: false,
         display: `minimal-ui`,
         icon: `src/images/joshunwin.png`, // This path is relative to the root of the site.
       },
